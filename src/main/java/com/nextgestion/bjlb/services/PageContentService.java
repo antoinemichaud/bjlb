06ab1587fbd92b9e7c4e80116a7019a30bad056e
Antoine MICHAUD <antoinemichaud34@gmail.com>
2013-08-04 00:35:07 +0200

    Add service for Joke content - WIP

---
+++ src/main/java/com/nextgestion/bjlb/services/PageContentService.java
@@ -0,0 +1,15 @@
+package com.nextgestion.bjlb.services;
+
+import com.nextgestion.bjlb.model.Joke;
+
+public class PageContentService {
+
+    public String getPageContent(Integer pageNumber) {
+        return null;
+    }
+
+    private Joke tempJokeCreation() {
+        return null;
+    }
+
+}
