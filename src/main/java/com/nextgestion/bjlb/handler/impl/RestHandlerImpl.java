36476ce214d703e76585aa9627fcf722f586bb4a
Antoine MICHAUD <antoinemichaud34@gmail.com>
2013-08-04 12:03:40 +0200

    vert.x configuration

--- src/main/java/com/nextgestion/bjlb/handler/impl/RestHandlerImpl.java
+++ src/main/java/com/nextgestion/bjlb/handler/impl/RestHandlerImpl.java
@@ -1,24 +1,42 @@
 package com.nextgestion.bjlb.handler.impl;
 
-import java.util.Map;
-
-import org.vertx.java.core.http.HttpServerRequest;
-
 import com.nextgestion.bjlb.handler.RestHandler;
+import com.nextgestion.bjlb.services.PageContentService;
+import org.vertx.java.core.MultiMap;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.HttpServerResponse;
 
 public class RestHandlerImpl implements RestHandler {
 
-	private static final String page = "page";
-	
-	@Override
-	public void handle(final HttpServerRequest event) {
-		
-		final Map<String, String> params = event.params();
-		
-		if (params.containsKey(page) && params.get(page) != null) {
-			event.response.write(params.get(page));
-			event.response.end();
-		}
-	}
+    private static final String page = "page";
+
+    private final PageContentService pageContentService;
+
+    public RestHandlerImpl(final PageContentService pageContentService) {
+        this.pageContentService = pageContentService;
+    }
+
+    @Override
+    public void handle(final HttpServerRequest request) {
+
+        final MultiMap params = request.params();
+
+        final String pageNumberStr = params.get(page);
+
+        if (pageNumberStr != null) {
+            final HttpServerResponse response = request.response();
+            try{
+                final int pageNumber = Integer.parseInt(pageNumberStr);
+                final String pageContent = pageContentService.getPageContent(pageNumber);
+                response.write(pageContent);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Probleme dans le format de l'adresse REST");
+            }
+            finally {
+                response.close();
+            }
+        }
+    }
 
 }
--- src/main/java/com/nextgestion/bjlb/handler/impl/StaticWebHandlerImpl.java
+++ src/main/java/com/nextgestion/bjlb/handler/impl/StaticWebHandlerImpl.java
@@ -1,26 +1,18 @@
 package com.nextgestion.bjlb.handler.impl;
 
-import org.vertx.java.core.http.HttpServerRequest;
-
 import com.nextgestion.bjlb.handler.StaticWebHandler;
+import org.vertx.java.core.http.HttpServerRequest;
 
 public class StaticWebHandlerImpl implements StaticWebHandler {
 
-	@Override
-	public void handle(final HttpServerRequest req) {
-		
-		final String file = "/".equals(req.path) ? "index.html" : req.path;
+    @Override
+    public void handle(final HttpServerRequest req) {
+
+        final String file = "/".equals(req.path()) ? "index.html" : req.path();
         final String fileRequiredPath = "webroot/" + file;
 
-        if (fileExists(fileRequiredPath)) {
-            req.response.sendFile(fileRequiredPath);
-        } else {
-            req.response.sendFile("webroot/Kickstrap/extras/root goodies/404.html");
-        }
-	}
-	
-	private boolean fileExists(String path) {
-        return StaticWebHandlerImpl.class.getResourceAsStream("/" + path) != null;
+        req.response().sendFile(fileRequiredPath, "webroot/Kickstrap/extras/root goodies/404.html");
+        req.response().close();
     }
 
 }
